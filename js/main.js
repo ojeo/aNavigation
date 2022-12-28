badc03d9e138ed77a582b4321b815be7395c3ac1
Je <jed.fang@dbappsecurity.com.cn>
2022-12-28 18:13:29 +0800

    菜单变成异步请求

--- js/main.js
+++ js/main.js
@@ -68,115 +68,17 @@ function rgb2hex(rgb) {
     function hex(x) {
         return ("0" + parseInt(x).toString(16)).slice(-2);
     }
+
     return "#" + hex(rgb[1]) + hex(rgb[2]) + hex(rgb[3]);
 }
+
 // rgb to hex结束
 
-// 添加书签
-$(function() {
-        var bookmark = {
-            data: [{
-                name: '网盘',
-                link: 'https://s.abbb.top:50/drive/',
-                box_shadow: '#35a8bd',
-                icon: 'img/ds-drive.png',
-            }, {
-                name: '照片',
-                link: 'https://s.abbb.top:50/moments/',
-                box_shadow: '#5dcbb6',
-                icon: 'img/ds-moments.png',
-            }, {
-                name: '电影',
-                link: 'https://s.abbb.top:50/video/',
-                box_shadow: '#d74440',
-                icon: 'img/ds-video.png',
-            }, {
-                name: '文件',
-                link: 'https://s.abbb.top:50/file/',
-                box_shadow: '#f4b040',
-                icon: 'img/ds-file.png',
-            }, {
-                name: '下载',
-                link: 'https://s.abbb.top:50/download/',
-                box_shadow: '#ed9135',
-                icon: 'img/ds-get.png',
-            }, {
-                name: 'DSM',
-                link: 'https://s.abbb.top:50/',
-                box_shadow: '#5f6fe8',
-                icon: 'img/synology.png',
-            }, {
-                name: 'Cnbeta',
-                link: 'https://www.cnbeta.com.tw/',
-                box_shadow: '#3776D2B8',
-                icon: 'img/cnbeta.png',
-            }, {
-                name: '知乎',
-                link: 'https://www.zhihu.com/',
-                box_shadow: '#0078D8',
-                icon: 'img/zhihulogo.png',
-            }, {
-                name: '凤凰网',
-                link: 'https://www.ifeng.com/',
-                box_shadow: '#62677b',
-                icon: 'img/ifeng.png',
-            }, {
-                name: '哔哩哔哩',
-                link: 'https://www.bilibili.com/',
-                box_shadow: '#ed4b76',
-                icon: 'img/bilibili2.png',
-            }, {
-                name: '樱花动漫',
-                link: 'https://www.yhdmz.org/',
-                box_shadow: '#ee7182',
-                icon: 'img/yinghua.png',
-            }, {
-                name: '电影天堂',
-                link: 'https://www.dy2018.com/',
-                box_shadow: '#62677b',
-                icon: 'img/dytt.png',
-            }, {
-                name: '今日热榜',
-                link: 'https://rebang.today/',
-                box_shadow: '#F2584A',
-                icon: 'img/headline.png',
-            // }, {
-            //     name: '虎扑',
-            //     link: 'https://www.hupu.com/',
-            //     box_shadow: '#c01d2f',
-            //     icon: 'img/hupu.png',
-            // }, {
-            //     name: 'CSDN',
-            //     link: 'https://www.csdn.net/',
-            //     box_shadow: '#c5000a',
-            //     icon: 'img/csdn.png',
-            }, {
-                name: '少数派',
-                link: 'https://sspai.com/',
-                box_shadow: '#D7191A',
-                icon: 'img/sspai.png',
-            }, {
-                name: '小众软件',
-                link: 'https://www.appinn.com/',
-                box_shadow: '#3279ea',
-                icon: 'img/xiaozhong.png',
-            }, {
-                name: '数字尾巴',
-                link: 'https://www.dgtle.com/',
-                box_shadow: '#62677b',
-                icon: 'img/shuziweiba.png',
-            // }, {
-            //     name: '疫情跟踪',
-            //     link: 'https://app.21jingji.com/html/2020yiqing/',
-            //     box_shadow: '#7C5DC7',
-            //     icon: 'img/ding.png',
-            // }, {
-            //     name: '疫情辟谣',
-            //     link: 'https://vp.fact.qq.com/home',
-            //     box_shadow: '#00A0E9',
-            //     icon: 'img/true.png',
-            }]
-        }
+
+// 添加书签结束
+$.ajax({
+    url: "js/bookmark", success: function (bookmark) {
+        bookmark = JSON.parse(bookmark)
         for (var i = 0; i < bookmark.data.length; i++) {
             if (bookmark.data[i].name == 'Via插件') {
                 var addList = '<li class="folder-item col-xs-3 col-sm-2 visible-xs visible-sm"><a target="_blank" href="' + bookmark.data[i].link + '"><div class="folder-item-box"><img class="folder-item-img" style="box-shadow:' + bookmark.data[i].box_shadow + ' 0 14px 12px -6px" src="' + bookmark.data[i].icon + '" /><p>' + bookmark.data[i].name + '</p></div></a></li>'
@@ -185,11 +87,26 @@ $(function() {
             }
             $('#folder ul').append(addList);
         }
-    })
-    // 添加书签结束
-
+    }
+});
+$.ajax({
+    url: "js/sidemenus", success: function (menus) {
+        menus = JSON.parse(menus)
+        var menus_html = "";
+        for (var i = 0; i < menus.data.length; i++) {
+            var group_name = menus.data[i].name
+            var item_html = "";
+            for (var j = 0; j < menus.data[i].items.length; j++) {
+                item_html += '<a target="_blank" href="' + menus.data[i].items[j].url + '"><li class="sidenav-btn">' + menus.data[i].items[j].name + '<p>' + menus.data[i].items[j].desc + '</p></li></a>';
+            }
+            var menu_html = '<blockquote class="blockquote">▎ ' + menus.data[i].name + '</blockquote><ul>' + item_html + '</ul>';
+            menus_html += menu_html;
+        }
+        $('#menu').prepend(menus_html);
+    }
+});
 // 搜索相关
-$(function() {
+$(function () {
     var search = {
         data: [{
             name: 'baidu',
@@ -311,78 +228,79 @@ $(function() {
     }
 
     // 切换搜索引擎
-    $(document).click(function(e) {
-            var id = e.target.id;
-            for (var i = 0; i < search.data.length; i++) {
-                if (id == search.data[i].name) {
-                    document.getElementById("state").innerHTML = "<a href='folder://'><img style='width:300px;' src='img/" + search.data[i].name + ".png'></a>";
-                    $('#submitButton').css('background-color', search.data[i].color); //按钮bg
-                    $('#Select').css('color', search.data[i].color); //选择器
-                    $('#nav').css('display', 'none');
-                    $('#folder').css('display', 'block');
-                    document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>";
-                    $("#inputText").attr("placeholder", search.data[i].placeholder);
-                    $("#form").attr("action", search.data[i].searchlink);
-                    $("#inputText").attr("name", search.data[i].searchname);
+    $(document).click(function (e) {
+        var id = e.target.id;
+        for (var i = 0; i < search.data.length; i++) {
+            if (id == search.data[i].name) {
+                document.getElementById("state").innerHTML = "<a href='folder://'><img style='width:300px;' src='img/" + search.data[i].name + ".png'></a>";
+                $('#submitButton').css('background-color', search.data[i].color); //按钮bg
+                $('#Select').css('color', search.data[i].color); //选择器
+                $('#nav').css('display', 'none');
+                $('#folder').css('display', 'block');
+                document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>";
+                $("#inputText").attr("placeholder", search.data[i].placeholder);
+                $("#form").attr("action", search.data[i].searchlink);
+                $("#inputText").attr("name", search.data[i].searchname);
 
-                    //存入用户数据
-                    var searchPho = $('#state a img').attr("src"); //搜索引擎头图
-                    var color = rgb2hex($('.submitButton').css('background-color')); //搜索按钮颜色和搜索框四边颜色
-                    var searchL = $('#form').attr("action"); //searchlink
-                    var searchN = $('#inputText').attr("name"); //searchname
-                    var placeholder = $('#inputText').attr('placeholder');
-                    storage.data = [searchPho, color, placeholder, searchL, searchN] //记录用户数据
-                        // console.log('新存入localStorage的数据：' + storage.data); //新存入localStorage的数据
-                    break;
-                }
+                //存入用户数据
+                var searchPho = $('#state a img').attr("src"); //搜索引擎头图
+                var color = rgb2hex($('.submitButton').css('background-color')); //搜索按钮颜色和搜索框四边颜色
+                var searchL = $('#form').attr("action"); //searchlink
+                var searchN = $('#inputText').attr("name"); //searchname
+                var placeholder = $('#inputText').attr('placeholder');
+                storage.data = [searchPho, color, placeholder, searchL, searchN] //记录用户数据
+                // console.log('新存入localStorage的数据：' + storage.data); //新存入localStorage的数据
+                break;
             }
-        })
-        // 搜索相关结束
+        }
+    })
+    // 搜索相关结束
 
     // 夜间模式
-    $('#night').click(function() {
-            if (rgb2hex($('#main').css('background-color')) == '#ffffff') {
-                $('#main').css('background-color', '#2f2f33'); //主界面
-                $('#menu').css('background-color', '#5C5C5C'); //侧栏
-                document.getElementById("night").innerHTML = "日间模式";
-                li.css({
-                    "background-color": "#575757",
-                    "color": "#DBDBDB"
-                });
-                blockquote.css('color', '#DBDBDB');
-            } else if (rgb2hex($('#main').css('background-color')) == '#2f2f33') {
-                $('#main').css('background-color', '#ffffff'); //主界面
-                $('#menu').css('background-color', '#EDEDED'); //侧栏
-                document.getElementById("night").innerHTML = "夜间模式";
-                li.css({
-                    "background-color": "#E3E3E3",
-                    "color": "black"
-                });
-                blockquote.css('color', 'black');
-            }
-            var mainbg = rgb2hex($('#main').css('background-color'));
-            var menubg = rgb2hex($('#menu').css('background-color'));
-            var inner = document.getElementById("night").innerHTML;
-            var libg = rgb2hex(li.css('background-color'));
-            var lico = rgb2hex(li.css('color'));
-            var blockquoteco = rgb2hex(blockquote.css('color'));
-            // storage.night = [mainbg, menubg, inner, libg, lico, blockquoteco];
-            // localStorage.clear()
-        })
-        // 夜间模式结束
+    $('#night').click(function () {
+        li = $('.sidenav-btn');
+        if (rgb2hex($('#main').css('background-color')) == '#ffffff') {
+            $('#main').css('background-color', '#2f2f33'); //主界面
+            $('#menu').css('background-color', '#5C5C5C'); //侧栏
+            document.getElementById("night").innerHTML = "日间模式";
+            li.css({
+                "background-color": "#575757",
+                "color": "#DBDBDB"
+            });
+            blockquote.css('color', '#DBDBDB');
+        } else if (rgb2hex($('#main').css('background-color')) == '#2f2f33') {
+            $('#main').css('background-color', '#ffffff'); //主界面
+            $('#menu').css('background-color', '#EDEDED'); //侧栏
+            document.getElementById("night").innerHTML = "夜间模式";
+            li.css({
+                "background-color": "#E3E3E3",
+                "color": "black"
+            });
+            blockquote.css('color', 'black');
+        }
+        var mainbg = rgb2hex($('#main').css('background-color'));
+        var menubg = rgb2hex($('#menu').css('background-color'));
+        var inner = document.getElementById("night").innerHTML;
+        var libg = rgb2hex(li.css('background-color'));
+        var lico = rgb2hex(li.css('color'));
+        var blockquoteco = rgb2hex(blockquote.css('color'));
+        // storage.night = [mainbg, menubg, inner, libg, lico, blockquoteco];
+        // localStorage.clear()
+    })
+    // 夜间模式结束
 
     // 更新提示框
     // $('#closealert').click(function() {
     //         var version = '4.1.2';
     //         localStorage.closealert = [version];
     //     })
-        // 更新提示框结束
+    // 更新提示框结束
 
 })
 
 // 显示/干掉壁纸
 var width = $(document).width();
-$('#bg').click(function() {
+$('#bg').click(function () {
     width < 768 ? ($('#main').css('background-image') == 'none' ? ($('#main').css('background-image', 'url(img/bg-xs.jpg)')) : ($('#main').css('background-image', 'none'))) : $('#main').css('background-image') == 'none' ? ($('#main').css('background-image', 'url(img/bg.jpg)')) : ($('#main').css('background-image', 'none'));
     var background = $('#main').css('background-image');
     storage.bg = [background];
@@ -397,6 +315,7 @@ function check() {
         return false;
     }
 }
+
 //检查搜索框是否为空结束
 
 //title问候语
@@ -420,40 +339,9 @@ if (time < 5) {
 function select() {
     $('#folder').css('display') == 'block' ? ($('#folder').css('display', 'none'), $('#nav').css('display', 'block'), document.getElementById("Select").innerHTML = "<hr>搜索引擎 <img src='img/search-change.svg?v=2ae7ab8'>") : ($('#nav').css('display', 'none'), $('#folder').css('display', 'block'), document.getElementById("Select").innerHTML = "<hr>书签 <img src='img/search-change.svg?v=2ae7ab8'>");
 }
+
 //导航、引擎选择器结束
 
-// 天气插件
-// (function(a, h, g, f, e, d, c, b) {
-//     b = function() {
-//         d = h.createElement(g);
-//         c = h.getElementsByTagName(g)[0];
-//         d.src = e;
-//         d.charset = "utf-8";
-//         d.async = 1;
-//         c.parentNode.insertBefore(d, c)
-//     };
-//     a["SeniverseWeatherWidgetObject"] = f;
-//     a[f] || (a[f] = function() {
-//         (a[f].q = a[f].q || []).push(arguments)
-//     });
-//     a[f].l = +new Date();
-//     if (a.attachEvent) {
-//         a.attachEvent("onload", b)
-//     } else {
-//         a.addEventListener("load", b, false)
-//     }
-// }(window, document, "script", "SeniverseWeatherWidget", "//cdn.sencdn.com/widget2/static/js/bundle.js?t=" + parseInt((new Date().getTime() / 100000000).toString(), 10)));
-// window.SeniverseWeatherWidget('show', {
-//     flavor: "bubble",
-//     location: "WX4FBXXFKE4F",
-//     geolocation: true,
-//     language: "auto",
-//     unit: "c",
-//     theme: "auto",
-//     token: "e14489a8-9a7e-477d-9c6c-b4b390175cca",
-//     hover: "enabled",
-//     container: "tp-weather-widget"
-// })
 
 // 搜索提示词
 class searchHint {
@@ -463,9 +351,11 @@ class searchHint {
         this.body = document.body;
         this.init();
     };
+
     init() {
         this.watchInput();
     };
+
     watchInput() {
         this.search.onkeyup = () => {
             if (this.search.value.length == 0) {
@@ -478,6 +368,7 @@ class searchHint {
             this.body.removeChild(script);
         }
     };
+
     showMsg(msg) {
         var href = $('#form').attr('action');
         var name = $('#inputText').attr('name');
@@ -492,16 +383,17 @@ class searchHint {
         if (str) { //有返回才显示#searchlist
             $('#searchlist').css("display", "block");
         }
-        document.onkeydown = function(event) {
+        document.onkeydown = function (event) {
             if (event.keyCode == 8 && v.length == 1) {
                 $('#searchlist').css("display", "none");
             }
         };
-        $(document).click(function() { //点击其他地方隐藏#searchlist
+        $(document).click(function () { //点击其他地方隐藏#searchlist
             $('#searchlist').css("display", "none");
         });
     }
 }
+
 const jsonp = new searchHint();
 
 /*
